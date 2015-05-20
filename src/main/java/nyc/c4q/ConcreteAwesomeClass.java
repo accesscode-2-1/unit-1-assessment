2f9a1514fff9da15853628b42dcec570818c775c
Pooja Pasawala <pooja@basicer.com>
2015-05-19 21:59:02 -0400

    in the middle of trying to fix java test 03.

--- src/main/java/nyc/c4q/ConcreteAwesomeClass.java
+++ src/main/java/nyc/c4q/ConcreteAwesomeClass.java
@@ -4,8 +4,22 @@ package nyc.c4q;
  * Created by amyquispe on 5/19/15.
  */
 public class ConcreteAwesomeClass extends AbstractAwesomeClass {
+    int data;
+
     public ConcreteAwesomeClass(){
+        this.data = 4;
     }
     public ConcreteAwesomeClass(int startData){
+        this.data = startData;
     }
+
+//    @Override
+//    public int getData() {
+//        return data;
+//    }
+//
+//    @Override
+//    public void setData(int someData) {
+//        this.data = someData;
+//    }
 }
