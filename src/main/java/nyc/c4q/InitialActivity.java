46413f0cc7625211e01254ef9341182955a90d38
Amcbri15 <anthony.mcbride0711@gmail.com>
2015-05-19 21:32:34 -0400

    9:30pm push

--- src/main/java/nyc/c4q/InitialActivity.java
+++ src/main/java/nyc/c4q/InitialActivity.java
@@ -2,6 +2,7 @@ package nyc.c4q;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
@@ -9,8 +10,12 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-public class InitialActivity extends Activity {
+public class InitialActivity extends Activity implements View.OnClickListener {
 
+  Button plus;
+  Button minus;
+  Button empty;
+  TextView tvCounter;
   public int counter = 0;
   public SharedPreferences preferences = null;
   public final static String TAG = "C4QTAG";
@@ -33,6 +38,34 @@ public class InitialActivity extends Activity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_initial);
+
+    plus = (Button)findViewById(R.id.buttonPlus);
+    minus = (Button)findViewById(R.id.buttonMinus);
+    empty = (Button)findViewById(R.id.buttonEmpty);
+    tvCounter = (TextView)findViewById(R.id.tvCounter);
+
+      plus.setOnClickListener(this);
+      minus.setOnClickListener(this);
+      empty.setOnClickListener(this);
+
     preferences = getPreferences(Context.MODE_PRIVATE);
   }
+
+  @Override
+  public void onClick(View v){
+    if (v == plus){
+        counter++;
+        tvCounter.setText(Integer.toString(counter));
+    }
+    if (v == minus){
+        counter--;
+        tvCounter.setText(Integer.toString(counter));
+    }
+    if (v == empty){
+        counter = 0;
+        tvCounter.setText(Integer.toString(counter));
+    }
+  }
+
+
 }
