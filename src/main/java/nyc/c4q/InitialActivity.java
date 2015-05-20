786bae32beb023549d779d19534d924913e050d5
charlyn23 <charlyn.buchanan@gmail.com>
2015-05-19 22:00:25 -0400

    did assignment

--- src/main/java/nyc/c4q/InitialActivity.java
+++ src/main/java/nyc/c4q/InitialActivity.java
@@ -9,11 +9,20 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-public class InitialActivity extends Activity {
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+
+public class InitialActivity extends Activity implements View.OnClickListener {
 
   public int counter = 0;
   public SharedPreferences preferences = null;
   public final static String TAG = "C4QTAG";
+    TextView tvCounter;
+    Button buttonPlus;
+    Button buttonMinus;
+
+
 
   public void loadState(){
     Log.d(TAG, "loadState()");
@@ -31,8 +40,46 @@ public class InitialActivity extends Activity {
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
+      Log.d(TAG, "onCreate()");
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_initial);
     preferences = getPreferences(Context.MODE_PRIVATE);
-  }
+
+      tvCounter = (TextView) findViewById(R.id.tvCounter);
+      buttonPlus = (Button) findViewById(R.id.buttonPlus);
+      buttonMinus = (Button) findViewById(R.id.buttonMinus);
+      Button buttonTileActivity = (Button) findViewById(R.id.buttonTileActivity);
+      Button buttonEmpty = (Button) findViewById(R.id.buttonEmpty);
+      loadState();
+      }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        saveState();    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        tvCounter.getText();
+    };
+
+
+
+    public void onClick (View v) {
+
+        Log.d(TAG, "onClick()");
+        if(v.getId()==R.id.buttonPlus){
+            counter += 1;
+            Log.d(TAG, "onClick().buttonPlus, counter="+counter);
+            tvCounter.setText(String.valueOf(counter));
+        }
+        if (v.getId()==R.id.buttonMinus) {
+            counter -= 1;
+            Log.d(TAG, "onClick().buttonMinus, counter="+counter);
+            tvCounter.setText(String.valueOf(counter));
+
+        }
+    }
+
 }
--- src/main/java/nyc/c4q/SubFunClass.java
+++ src/main/java/nyc/c4q/SubFunClass.java
@@ -4,6 +4,7 @@ package nyc.c4q;
  * Created by amyquispe on 5/19/15.
  */
 public class SubFunClass {
-    public SubFunClass(){
+    public SubFunClass (){
+        SubFunClass funObject = new SubFunClass();
     }
 }
