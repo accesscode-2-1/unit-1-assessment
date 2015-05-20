9302d189392c863d872cb8194f5e646f03d2d157
Luke Lee <samsung3star@gmail.com>
2015-05-19 21:14:27 -0400

    finished Java

--- src/main/java/nyc/c4q/AwesomeContainer.java
+++ src/main/java/nyc/c4q/AwesomeContainer.java
@@ -2,21 +2,20 @@ package nyc.c4q;
 
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Iterator;
 import java.util.List;
+
 
 /**
  * Created by amyquispe on 5/19/15.
  */
 public class AwesomeContainer {
-    public static Collection createAwesomeContainer(){
-        List<AwesomeInterface> list = new ArrayList<AwesomeInterface>();
+    public static ArrayList<AwesomeInterface> createAwesomeContainer(){
+        ArrayList list = new ArrayList<AwesomeInterface>();
         return list;
     }
 
     public static void addAwesomeObject(Collection awesomeContainer){
-        awesomeContainer = AwesomeContainer.createAwesomeContainer();
         awesomeContainer.add(new SubFunClass());
-        return ;
+
     }
 }
