513408bc854c694d75af506c3976bb040970486a
Shadow Slim <reinardcox@aol.com>
2015-05-26 22:00:21 -0400

    Part II: Assesment test.

--- src/main/java/nyc/c4q/InitialActivity.java
+++ src/main/java/nyc/c4q/InitialActivity.java
@@ -2,6 +2,7 @@ package nyc.c4q;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
@@ -10,62 +11,73 @@ import android.widget.Button;
 import android.widget.TextView;
 
 public class InitialActivity extends Activity {
+    Intent intent = new Intent(this, InitialActivity.class);
 
-  public int counter = 0;
-  public SharedPreferences preferences = null;
-  public final static String TAG = "C4QTAG";
+    public final static String TAG = "C4QTAG";
+    public int counter = 0;
+    public SharedPreferences preferences = null;
 
-  public void loadState(){
-    Log.d(TAG, "loadState()");
-    counter = preferences.getInt("counter", 0);
-    Log.d(TAG, "loadState(): counter=="+counter);
-  }
+    public void loadState() {
+        Log.d(TAG, "loadState()");
+        counter = preferences.getInt("counter", 0);
+        Log.d(TAG, "loadState(): counter==" + counter);
+    }
 
-  public void saveState(){
-    Log.d(TAG, "saveState()");
-    Log.d(TAG, "saveState(): counter=="+counter);
-    SharedPreferences.Editor editor = preferences.edit();
-    editor.putInt("counter", counter);
-    editor.commit();
-  }
+    public void saveState() {
+        Log.d(TAG, "saveState()");
+        Log.d(TAG, "saveState(): counter==" + counter);
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.putInt("counter", counter);
+        editor.commit();
+    }
 
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_initial);
-    preferences = getPreferences(Context.MODE_PRIVATE);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_initial);
+        preferences = getPreferences(Context.MODE_PRIVATE);
 
-    Button buttonPlus = (Button) findViewById(R.id.buttonPlus);
-    Button buttonMinus = (Button) findViewById(R.id.buttonMinus);
-    final TextView tvCounter = (TextView) findViewById(R.id.tvCounter);
-
-    buttonPlus.setOnClickListener(new View.OnClickListener() {
-      @Override
-      public void onClick(View v) {
-        counter++;
+        Button buttonPlus = (Button) findViewById(R.id.buttonPlus);
+        Button buttonMinus = (Button) findViewById(R.id.buttonMinus);
+        Button buttonTileActivity = (Button) findViewById(R.id.buttonTileActivity);
 
 
-        tvCounter.setText(Integer.toString(counter));
-
-      }
-    });
+        final TextView tvCounter = (TextView) findViewById(R.id.tvCounter);
 
 
-    buttonMinus.setOnClickListener(new View.OnClickListener() {
-      @Override
-      public void onClick(View v) {
-        counter--;
+        buttonPlus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                counter++;
+                tvCounter.setText(Integer.toString(counter));
 
+            }
+        });
 
-        tvCounter.setText(Integer.toString(counter));
+        buttonMinus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                counter--;
+                tvCounter.setText(Integer.toString(counter));
+            }
+        });
 
-      }
-    });
+        buttonTileActivity.setOnClickListener(new View.OnClickListener() {
 
+            @Override
+            public void onClick(View v) {
 
+                startActivity(intent);
 
+            }
+        });
 
+    }
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        super.onSaveInstanceState(savedInstanceState);
+        savedInstanceState.putInt("counter", counter);
+        saveState();
+    }
 
-
-  }
 }
