47073f669443348e57706c8abc7a25f9d925dd43
jvvlives2005 <jvvlives2005@aol.com>
2015-05-26 21:58:04 -0400

    incomplete assessment-part1

--- src/main/java/nyc/c4q/AwesomeContainer.java
+++ src/main/java/nyc/c4q/AwesomeContainer.java
@@ -9,7 +9,7 @@ import java.util.ListIterator;
 /**
  * Created by amyquispe on 5/19/15.
  */
-public class AwesomeContainer implements Collection {
+public class AwesomeContainer {
 
 
     public static void addAwesomeObject(Collection awesomeContainer){
@@ -17,70 +17,6 @@ public class AwesomeContainer implements Collection {
     }
 
     public static void createAwesomeContainer() {
-    }
-
-    @Override
-    public boolean containsAll(Collection collection) {
-        return false;
-    }
-
-    @Override
-    public boolean add(Object o) {
-        return false;
-    }
-
-    @Override
-    public boolean addAll(Collection collection) {
-        return false;
-    }
-
-    @Override
-    public void clear() {
 
     }
-
-    @Override
-    public boolean contains(Object o) {
-        return false;
-    }
-
-    @Override
-    public boolean isEmpty() {
-        return false;
-    }
-
-    @Override
-    public Iterator iterator() {
-        return null;
-    }
-
-    @Override
-    public boolean remove(Object o) {
-        return false;
-    }
-
-    @Override
-    public int size() {
-        return 0;
-    }
-
-    @Override
-    public Object[] toArray() {
-        return new Object[0];
-    }
-
-    @Override
-    public Object[] toArray(Object[] objects) {
-        return Object[0];
-    }
-
-    @Override
-    public boolean retainAll(Collection collection) {
-        return false;
-    }
-
-    @Override
-    public boolean removeAll(Collection collection) {
-        return false;
-    }
-}
+ }
